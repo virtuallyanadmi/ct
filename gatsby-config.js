02281b3434d4f76bf88c101d982351fdc26bbbf4
Jonathan Stewart <jonathan@virtuallyanadmin.com>
2024-12-09 16:45:32 -0600

    Finished Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,10 +3,17 @@
  */
 module.exports = {
   siteMetadata: {
-    title: "My First Gatsby Site",
+    title: "My Site",
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 };
